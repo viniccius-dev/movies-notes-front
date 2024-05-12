6f894607ff7858506698b0b5ecacbed7f39cf1d6
Vinícius <santsmarcos81@gmail.com>
2024-05-12 17:35:01 -0300

    Configurando tema global e fonte

---
+++ src/styles/theme.js
@@ -0,0 +1,24 @@
+export default {
+    COLORS: {
+      BACKGROUND_600: "#0D0C0F",
+      BACKGROUND_500: "#1C1B1E",
+      BACKGROUND_400: "#282124",
+      BACKGROUND_300: "#262529",
+      BACKGROUND_200: "#312E38",
+      BACKGROUND_100: "#3E3B47",
+  
+      WHITE_100: "#FFFFFF",
+      WHITE_200: "#F4EDE8",
+      WHITE_300: "#E5E5E5",
+
+      ORANGE: "#FF9000",
+      PINK: "#FF859B",
+      
+      GRAY_400: "#666360",
+      PINK_300: "#948F99",
+      GRAY_200: "#999591",
+      GRAY_100: "#CAC4CF",
+  
+      RED: "#FF002E"
+    }
+  }
